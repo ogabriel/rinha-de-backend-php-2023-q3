65e2fae8e64c2c2bc65a1958ce93c025dd37bd56
ogabriel <gabrieloliver8991@gmail.com>
2023-12-29 18:29:17 -0300

    fix api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PessoaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/pessoas', [PessoaController::class, 'store']);
+Route::get('/pessoas/{id}', [PessoaController::class, 'show']);
+Route::get('/pessoas', [PessoaController::class, 'search']);
+Route::get('/contagem-pessoas', [PessoaController::class, 'count']);
