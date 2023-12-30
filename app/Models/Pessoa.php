e6e1051c5a740448eb43669c0423507584d8eef9
ogabriel <gabrieloliver8991@gmail.com>
2023-12-29 23:12:42 -0300

    add getter and setter for stack because laravel do not have this feature...

--- app/Models/Pessoa.php
+++ app/Models/Pessoa.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,14 @@ class Pessoa extends Model
     use HasUuids;
 
     public $timestamps = false;
+
+    protected function stack(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => json_decode(str_replace(['{', '}'], ['[', ']'], $value)),
+            set: fn (array $value) => str_replace(['[', ']'], ['{', '}'], json_encode($value)),
+        );
+    }
 
     protected $casts = [
         'stack' => 'array',
