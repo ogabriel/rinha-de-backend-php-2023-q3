d4a0210fec70b6ff3b4ad77decda61a569c87c20
ogabriel <gabrieloliver8991@gmail.com>
2023-12-29 22:22:51 -0300

    fix docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,8 @@ if [ "$1" = 'release' ]; then
 elif [ "$1" = 'migrate_and_release' ]; then
     make database-check
 
-    php /app/artisan migrate:fresh
+    PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USERNAME -c "CREATE DATABASE $DB_DATABASE"
+    php /app/artisan migrate:fresh --force
 
     exec php /app/artisan serve --host=0.0.0.0 --port=$PORT
 fi
