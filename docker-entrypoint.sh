484bb02b4b223a7c3c2cdc6efec0e79823acaca9
ogabriel <gabrieloliver8991@gmail.com>
2023-12-29 21:39:58 -0300

    remove duplicated migrations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,6 @@ elif [ "$1" = 'migrate_and_release' ]; then
     make database-check
 
     php /app/artisan migrate:fresh
-    php /app/artisan migrate
 
     exec php /app/artisan serve --host=0.0.0.0 --port=$PORT
 fi
