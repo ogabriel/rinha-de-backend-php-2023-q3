c919729f3ce62b194a762bfdb21b0fe15d358f59
ogabriel <gabrieloliver8991@gmail.com>
2023-12-29 22:36:43 -0300

    fix pessoa model

--- app/Models/Pessoa.php
+++ app/Models/Pessoa.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Pessoa extends Model
 {
-    use HasFactory;
+    use HasUuids;
 
     public $timestamps = false;
+
+    protected $casts = [
+        'education' => 'array',
+    ];
 }
