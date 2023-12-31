7c4dfcc8b939371dd9020401aa67c612740b6939
ogabriel <gabrieloliver8991@gmail.com>
2023-12-31 08:44:50 -0300

    artisan optimize on docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@
 if [ "$1" = 'release' ]; then
     make database-check
 
+    php artisan optimize
+
     # exec php artisan serve --host=0.0.0.0 --port=$PORT
     exec supervisord -c /app/supervisord.conf
 elif [ "$1" = 'migrate_and_release' ]; then
@@ -10,6 +12,8 @@ elif [ "$1" = 'migrate_and_release' ]; then
 
     PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USERNAME -c "CREATE DATABASE $DB_DATABASE"
     php /app/artisan migrate:fresh --force
+
+    php artisan optimize
 
     # exec php artisan serve --host=0.0.0.0 --port=$PORT
     exec supervisord -c /app/supervisord.conf
