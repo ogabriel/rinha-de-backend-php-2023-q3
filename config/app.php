9fa7a2ec4c4362c08e99ecac1ab2e0b055a46e7a
ogabriel <gabrieloliver8991@gmail.com>
2023-12-31 08:50:37 -0300

    delete AuthServiceProvider

--- config/app.php
+++ config/app.php
@@ -164,7 +164,6 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
